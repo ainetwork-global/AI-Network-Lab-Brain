f2c4ea7fd09314e2e9fd4b1a38b241bb8cb5e63d
Gilson Begatti <gilsonbegatti@gmail.com>
2026-07-15 17:51:05 -0300

    Filter non-bounty Algora links

---
+++ 01_GLOBAL_REVENUE_BRAIN/10_SCRIPTS/algora_quality_filter.py
@@ -0,0 +1,46 @@
+﻿import sqlite3
+
+conn = sqlite3.connect("11_DATA/global_revenue_brain.db")
+
+conn.execute("""
+DELETE FROM algora_open_bounties
+WHERE
+    algora_url LIKE 'tel:%'
+    OR algora_url LIKE '%cal.com%'
+    OR algora_url LIKE '%x.com/%'
+    OR algora_url LIKE '%twitter.com/%'
+    OR algora_url LIKE '%linkedin.com%'
+    OR algora_url LIKE '%youtube.com%'
+    OR algora_url LIKE '%medium.com%'
+    OR algora_url LIKE '%blog%'
+    OR (
+        github_url IS NULL
+        AND algora_url NOT LIKE 'https://algora.io/%/bounties/%'
+    );
+""")
+
+conn.commit()
+
+print()
+print("===== ALGORA QUALITY FILTER =====")
+
+for row in conn.execute("""
+SELECT
+organization,
+COUNT(*)
+FROM algora_open_bounties
+GROUP BY organization
+ORDER BY COUNT(*) DESC;
+"""):
+    print(row)
+
+print()
+
+print("TOTAL:")
+print(
+    conn.execute(
+        "SELECT COUNT(*) FROM algora_open_bounties"
+    ).fetchone()[0]
+)
+
+conn.close()
