0d2ac7140c8b866b1b4ad6cb0fbd44239a80c425
Gilson Begatti <gilsonbegatti@gmail.com>
2026-07-16 15:22:25 -0300

    Create revenue execution queue

---
+++ 01_GLOBAL_REVENUE_BRAIN/05_SETTLEMENT/create_execution_queue.py
@@ -0,0 +1,60 @@
+﻿import sqlite3
+
+DB="../brain.db"
+
+con=sqlite3.connect(DB)
+cur=con.cursor()
+
+cur.execute("""
+
+CREATE VIEW IF NOT EXISTS revenue_execution_queue AS
+
+SELECT
+
+o.id,
+
+o.title,
+
+o.reward_usd,
+
+o.source_name,
+
+s.payment_method,
+
+s.currency,
+
+s.settlement_target,
+
+s.automation_level,
+
+CASE
+
+WHEN s.payment_method='wallet' THEN 100
+
+WHEN s.payment_method='stripe' THEN 90
+
+WHEN s.payment_method='nomad' THEN 80
+
+ELSE 50
+
+END
+
++
+
+COALESCE(o.reward_usd,0)/100.0
+
+AS execution_priority
+
+FROM opportunities o
+
+LEFT JOIN settlement_methods s
+
+ON s.opportunity_id=o.id
+
+ORDER BY execution_priority DESC;
+
+""")
+
+con.commit()
+
+print("REVENUE EXECUTION QUEUE READY")
